b097dbb6825d470afda2d9f8a87a5373cb29a9dd
hjinor <76686851+guwls@users.noreply.github.com>
2025-04-05 14:08:13 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.05 ms, Memory: 81.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] numbers) {
+        int sum = 0;
+        
+        Set<Integer> set = new HashSet<>();
+        
+        for (int num : numbers) {
+            set.add(num);
+        }
+        
+        for (int i = 0; i <= 9; i++) {
+            if (!set.contains(i)) {
+                sum += i;
+            }
+        }
+        
+        return sum;
+    }
+}
